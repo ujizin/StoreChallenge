2cd9473b0da41ca62328085276b6f9012588babd
ujizin <lucas.yuji@enjoei.com.br>
2023-09-27 11:06:19 -0300

    feat: add image loader cache

--- app/src/main/kotlin/com/ujizin/storechallenge/StoreChallengeApplication.kt
+++ app/src/main/kotlin/com/ujizin/storechallenge/StoreChallengeApplication.kt
@@ -1,7 +1,28 @@
 package com.ujizin.storechallenge
 
 import android.app.Application
+import coil.ImageLoader
+import coil.ImageLoaderFactory
+import coil.disk.DiskCache
+import coil.memory.MemoryCache
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class StoreChallengeApplication : Application()
+class StoreChallengeApplication : Application(), ImageLoaderFactory {
+    override fun newImageLoader(): ImageLoader {
+        return ImageLoader.Builder(this)
+            .memoryCache {
+                MemoryCache.Builder(this)
+                    .maxSizePercent(0.25)
+                    .build()
+            }
+            .diskCache {
+                DiskCache.Builder()
+                    .directory(cacheDir.resolve("image_cache"))
+                    .maxSizeBytes(5 * 1024 * 1024)
+                    .build()
+            }
+            .respectCacheHeaders(false)
+            .build()
+    }
+}
